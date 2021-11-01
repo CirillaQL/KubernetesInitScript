68265d2011149f356d809bb893e9cc0928fa9667
lu.qian <lu.qian@easystack.cn>
2021-11-01 20:02:06 +0800

    init Project

---
+++ KuberneteInit.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+sudo apt-get update
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository \
+   "deb [arch=amd64] https://mirrors.ustc.edu.cn/docker-ce/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io
+
+sudo touch /etc/docker/daemon.json
+sudo echo -e "{\n    \"exec-opts\": [ \"native.cgroupdriver=systemd\"]\n}" >> /etc/docker/daemon.json
+
+sudo apt install apt-transport-https curl
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add
+sudo apt-add-repository "deb http://apt.kubernetes.io/ kubernetes-xenial main"
+sudo apt install kubeadm kubelet kubectl kubernetes-cni
+
+sudo kubeadm init
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+
+kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
---
+++ README.md
@@ -0,0 +1,3 @@
+# Kubernetes 安装脚本
+
+本脚本是自用记录安装Kubernetes的过程，其中网络环境为无墙环境
